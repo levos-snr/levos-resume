39dc3bf241935c96588782e407d2176638956ed9
Lewis Odero <lewisodero27@gmail.com>
2022-09-19 21:37:56 +0300

    my-resume

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Lewis Odero',
     description:
       'Lewis Odero is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://lewisodero.me', // No trailing slash allowed!
+    siteUrl: 'https://lewisodero.me/', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@levos_snr',
   },
